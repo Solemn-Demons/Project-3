5ddf9577464d76b4cf9272c5c8531c7c949288f6
jackjg123 <131559096+jackjg123@users.noreply.github.com>
2023-10-04 19:24:19 -0400

    Added typeDefs types for Profile Auth Query mutation

--- Server/schemas/typeDefs.js
+++ Server/schemas/typeDefs.js
@@ -23,9 +23,6 @@ const typeDefs = `
     addProfile(name: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
 
-    addSkill(profileId: ID!, skill: String!): Profile
-    removeProfile: Profile
-    removeSkill(skill: String!): Profile
   }
 `;
 
