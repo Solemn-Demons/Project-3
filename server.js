ad786a4155ef2f9d90ddb897741390a3ae9b1cd3
Abdikalik Dini <adini18@gmail.com>
2023-10-11 18:45:42 -0400

    setup basic qraphql server

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+const { GraphQLSchema} = require('graphql');
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+
+const RootQuery = require('./graphql/types').RootQuery;
+Mutation = require('./graphql/mutations').Mutation;
+
+const schema = new GraphQLSchema({
+    query: RootQuery,
+    mutation: Mutation
+});
+
+app.use('/graphql', graphqlHTTP ({
+    schema: schema,
+    graphiql:true
+}));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
