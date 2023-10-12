21acd5b1fbc836001e4024a2473bd18fa3724e68
jackjg123 <131559096+jackjg123@users.noreply.github.com>
2023-10-11 20:27:31 -0400

    Added posiible Plot Model

--- Server/models/index.js
+++ Server/models/index.js
@@ -1,1 +1,6 @@
 // require each model
+const User = require('./User');
+const Character = require('./Character');
+
+// export models
+module.exports = { User, Character };
--- Server/seeders/seeds.js
+++ Server/seeders/seeds.js
@@ -1,9 +1,80 @@
-// Import the database connection module and necessary models
+const db = require('./connection');
+const { User, Character } = require('../models');
+const cleanDB = require('./cleanDB');
 
-// Import the cleanDB function to remove existing data
+db.once('open', async () => {
+  await cleanDB('Character', 'characters');
+  await cleanDB('User', 'users');
 
-// Listen for the 'open' event on the database connection
+  // Seed characters
+  const characters = await Character.insertMany([
+    {
+      name: 'John Doe',
+      age: 30,
+      gender: 'Male',
+      physicalDescription: 'Tall with brown hair',
+      occupation: 'Software Engineer',
+    },
+    {
+      name: 'Jane Smith',
+      age: 28,
+      gender: 'Female',
+      physicalDescription: 'Short with blonde hair',
+      occupation: 'Doctor',
+    },
+    {
+      name: 'David Johnson',
+      age: 45,
+      gender: 'Male',
+      physicalDescription: 'Medium height, bald',
+      occupation: 'Accountant',
+    },
+    {
+      name: 'Emily Davis',
+      age: 35,
+      gender: 'Female',
+      physicalDescription: 'Athletic build, red hair',
+      occupation: 'Fitness Trainer',
+    },
+    // Add more character objects as needed
+  ]);
 
-// Clean the model collections, then seed them with the imported data
+  console.log('Characters seeded');
 
-// Create new model document
+  // Seed users
+  await User.create({
+    firstName: 'Alice',
+    lastName: 'Johnson',
+    email: 'alice@testmail.com',
+    password: 'password12345',
+    // Include any additional user data or orders if needed
+  });
+
+  await User.create({
+    firstName: 'Bob',
+    lastName: 'Smith',
+    email: 'bob@testmail.com',
+    password: 'password12345',
+    // Include any additional user data or orders if needed
+  });
+
+  await User.create({
+    firstName: 'Eve',
+    lastName: 'Williams',
+    email: 'eve@testmail.com',
+    password: 'password12345',
+    // Include any additional user data or orders if needed
+  });
+
+  await User.create({
+    firstName: 'Charlie',
+    lastName: 'Brown',
+    email: 'charlie@testmail.com',
+    password: 'password12345',
+    // Include any additional user data or orders if needed
+  });
+
+  console.log('Users seeded');
+
+  process.exit();
+});
