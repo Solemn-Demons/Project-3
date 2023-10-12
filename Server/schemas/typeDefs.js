3d1974c459f1eba4d00cf07175c9cc21e7cce29f
Abdikalik Dini <adini18@gmail.com>
2023-10-12 09:41:25 -0400

    added def types for user,character,plot, query and mutations

--- Server/schemas/typeDefs.js
+++ Server/schemas/typeDefs.js
@@ -1,35 +1,48 @@
-const {
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLID,
-  GraphQLSchema
-} = require('graphql');
+const { gql } = require('apollo-server-express');
 
-const CharacterType = new GraphQLObjectType({
-  name: 'Character',
-  fields: {
-      id: { type: GraphQLID },
-      name: { type: GraphQLString },
-      role: { type: GraphQLString },
-      description: { type: GraphQLString }
+const typeDefs = gql`
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
   }
-});
 
-
-const PlotType = new GraphQLObjectType({
-  name: 'Plot',
-  fields: {
-      id: { type: GraphQLID },
-      title: { type: GraphQLString },
-      description: { type: GraphQLString },
-      mainCharacter: {
-          type: CharacterType,
-          resolve(parent, args) {
-          }
-      },
-      timeline: { type: GraphQLString },  
-      setting: { type: GraphQLString }    
+  type Character {
+    id: ID!
+    name: String!
+    age: Int
+    gender: String
+    physicalDescription: String
+    occupation: String
   }
-});
+
+  type Plot {
+    id: ID!
+    title: String!
+    description: String
+    mainCharacter: Character
+    timeline: String
+    setting: String
+  }
+
+  type Query {
+    user(userId: ID!): User
+    character(characterId: ID!): Character
+    plot(plotId: ID!): Plot
+    storyboard(storyboardId: ID!): Storyboard
+  }
+
+  type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): User
+    addCharacter(name: String!, age: Int, gender: String, physicalDescription: String, occupation: String): Character
+    addPlot(title: String!, description: String, mainCharacterId: ID, timeline: String, setting: String): Plot
+    addStoryboard(storyboardText: String!, storyboardAuthor: String!): Storyboard
+    deleteStoryboard(storyboardId: ID!): Storyboard
+    updateStoryboard(storyboardId: ID!, storyboardText: String, storyboardAuthor: String): Storyboard
+    addComment(storyboardId: ID!, commentText: String!): Storyboard
+  }
+`;
 
 module.exports = typeDefs;
