6bbc8638024505e62b47bc6473cc4b963041c6b4
Abdikalik Dini <adini18@gmail.com>
2023-10-16 21:03:07 -0400

    updated user models and server.js and added bcrypt package

--- Server/models/User.js
+++ Server/models/User.js
@@ -1,17 +1,7 @@
-// Importing mongoose library for creating MongoDB schemas
 const mongoose = require('mongoose');
-
-// Destructuring Schema from mongoose
-const { Schema } = mongoose;
-
-// Importing bcrypt library for password hashing
 const bcrypt = require('bcrypt');
 
-// Importing Order schema from Order.js file
-const Order = require('./Order');
-
-// Defining user schema
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
     required: true,
@@ -32,7 +22,6 @@ const userSchema = new Schema({
     required: true,
     minlength: 5,
   },
-  orders: [Order.schema],
 });
 
 // Set up pre-save middleware to create password hash
@@ -41,17 +30,14 @@ userSchema.pre('save', async function (next) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
-
   next();
 });
 
 // Compare the incoming password with the hashed password
 userSchema.methods.isCorrectPassword = async function (password) {
-  await bcrypt.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 
-// Creating User model from userSchema
 const User = mongoose.model('User', userSchema);
 
-// Exporting User model
 module.exports = User;
