da3877592815eee784946b0dbb203a86ab8b60bc
Abdikalik Dini <adini18@gmail.com>
2023-10-16 20:47:38 -0400

    added imports to server.js

--- Server/server.js
+++ Server/server.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
-const { GraphQLSchema} = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
+const { GraphQLSchema } = require('graphql');
+const { User, Character, Plot } = require('./models'); // Adjust the path as needed
+const RootQuery = require('./graphql/types').RootQuery;
+const Mutation = require('./graphql/mutations').Mutation;
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-
-const RootQuery = require('./graphql/types').RootQuery;
-Mutation = require('./graphql/mutations').Mutation;
-
 const schema = new GraphQLSchema({
-    query: RootQuery,
-    mutation: Mutation
+  query: RootQuery,
+  mutation: Mutation,
 });
 
-app.use('/graphql', graphqlHTTP ({
-    schema: schema,
-    graphiql:true
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true,
 }));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
