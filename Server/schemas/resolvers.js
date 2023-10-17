80d882ee585c4edf30ce5975e7446d2e89af918c
Abdikalik Dini <adini18@gmail.com>
2023-10-16 20:26:36 -0400

    added resolver.js

--- Server/schemas/resolvers.js
+++ Server/schemas/resolvers.js
@@ -1,13 +1,61 @@
-// Define resolvers object
+const { User, Character, Plot } = require('../models'); 
 
-//query resolvers
+const resolvers = {
+  Query: {
+    user: async (_, { userId }) => {
+      try {
+        const user = await User.findById(userId);
+        return user;
+      } catch (error) {
+        throw new Error('Unable to fetch user');
+      }
+    },
+    character: async (_, { characterId }) => {
+      try {
+        const character = await Character.findById(characterId);
+        return character;
+      } catch (error) {
+        throw new Error('Unable to fetch character');
+      }
+    },
+    plot: async (_, { plotId }) => {
+      try {
+        const plot = await Plot.findById(plotId);
+        return plot;
+      } catch (error) {
+        throw new Error('Unable to fetch plot');
+      }
+    },
+  },
+  Mutation: {
+    addUser: async (_, { firstName, lastName, email, password }) => {
+      try {
+        const user = new User({ firstName, lastName, email, password });
+        await user.save();
+        return user;
+      } catch (error) {
+        throw new Error('Unable to create user');
+      }
+    },
+    addCharacter: async (_, { name, age, gender, physicalDescription, occupation }) => {
+      try {
+        const character = new Character({ name, age, gender, physicalDescription, occupation });
+        await character.save();
+        return character;
+      } catch (error) {
+        throw new Error('Unable to create character');
+      }
+    },
+    addPlot: async (_, { title, description, mainCharacterId, timeline, setting }) => {
+      try {
+        const plot = new Plot({ title, description, mainCharacter: mainCharacterId, timeline, setting });
+        await plot.save();
+        return plot;
+      } catch (error) {
+        throw new Error('Unable to create plot');
+      }
+    },
+  },
+};
 
-// Resolver to retrieve all instances of model
-
-// Resolver to retrieve a single instance of model by id
-
-// Mutation resolvers
-
-// resolver to create a new instance of model
-
-// resolver to remove a model by its id
+module.exports = resolvers;
