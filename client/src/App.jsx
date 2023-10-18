aba8c3c5b9f87db9857408a066b82cfe4db83f3d
Abdikalik Dini <adini18@gmail.com>
2023-10-18 10:54:42 -0400

    added apolloclient

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import client from './utils/apolloClient';  
+import { ApolloProvider } from '@apollo/client';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <ApolloProvider client={client}>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,8 +30,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
+    </ApolloProvider>
+  );
 }
 
-export default App
+export default App;
