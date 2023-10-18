70b852235486572567588dfe28512b93181b272f
Solemn-Demons <128663691+Solemn-Demons@users.noreply.github.com>
2023-10-18 17:25:47 -0400

    fixing css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';  
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/server';
 import App from './App.jsx';
-import './home.css';
+import './nav.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
